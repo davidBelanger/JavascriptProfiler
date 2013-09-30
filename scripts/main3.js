ebd5f25be55b5b4d5807fdb6e213afa3fa26692e
David Belanger <david.b.belanger@gmail.com>
2013-09-29 22:14:43 -0400

    updates

--- scripts/main3.js
+++ scripts/main3.js
@@ -9,7 +9,24 @@ console.log("here");
 
 console.log("got " + spectralnorm(10));
 
-var jobs = new Array(new Array(spectralnorm, 10, 1.2718440192507248), new Array(spectralnorm, 10, 1.8718440192507248));
+var jobs = new Array();
+spectTrue = spectralnorm(10);
+
+//Going forward, the use case for this will be that we will pass it spectralnorm, and then spectralnorm_instrumented. We will obtain spectTrue in the same way, and assert that the output
+//of spectralnorm_instrumented(10) = spectTrue
+
+//right now, since we don't have a means to create the alternative javascript, I'm just perturbing the target value and making sure that the tests fail. 
+
+jobs.push(new Array(spectralnorm, 10, spectTrue,"spect-should-pass"));
+jobs.push(new Array(spectralnorm, 10, spectTrue + .001,"spect-should-fail"));
+
+
+nbdTrue = runNBody(10);
+jobs.push(new Array(runNBody, 10, 2*nbdTrue,"nbody-should-fail"));
+jobs.push(new Array(runNBody, 10, nbdTrue,"nbody-should-pass"));
+
+
+
 console.log("running tests");
 doTests(jobs);
 
--- scripts/testing.js
+++ scripts/testing.js
@@ -2,23 +2,19 @@ function doTests(jobs){
     for(i = 0; i < jobs.length; i++){
 	thisJob = jobs[i];
 	test(thisJob);
-	analyze(thisJob);
     }
 
 }
 
-function analyze(thisJob){
-    console.log("expected " + thisJob[2]);
-    console.log("got" + thisJob[0](thisJob[1]));
-}
 
 function test(thisJob){
-    equal(thisJob[0](thisJob[1]),thisJob[2]);
-	
-}
+    var got = thisJob[0](thisJob[1]);
+    var expected = thisJob[2];
 
-function equal(a,b){
-    if(a !== b){
-	console.log("WARNING: TEST FAILED");
+    if(got != expected){
+	console.log(" for " + thisJob[3] +  ": expected " + expected + ", but got " + got);
+    }else{
+	console.log(thisJob[3] + " passed" );
     }
+	
 }
